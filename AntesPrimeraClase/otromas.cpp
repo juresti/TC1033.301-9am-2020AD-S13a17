38943c48db9a82229cdde7273f111644179af3fb
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2020-10-28 16:13:00 -0600

    Pruebas de VSC

---
+++ AntesPrimeraClase/otromas.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Otro para ver si se puede usar el mismo build" << endl;
+    return 0;
+}
---
+++ AntesPrimeraClase/prueba.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Antes de la primera clase" << endl; 
+}
